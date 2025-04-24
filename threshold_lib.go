1de5a9f49f443a3cbac47a02cb1c7eecf9798764
mutalisk999 <81849682@qq.com>
2025-04-24 15:01:34 +0800

    runtime.Pinner & return pointer of C.char

--- threshold_lib.go
+++ threshold_lib.go
@@ -12,18 +12,36 @@ import (
 	"github.com/okx/threshold-lib/tss/ecdsa/keygen"
 	"github.com/okx/threshold-lib/tss/ecdsa/sign"
 	"github.com/okx/threshold-lib/tss/key/reshare"
+	"runtime"
+	"unsafe"
 )
 
 type ErrString string
+
+//export FreeCString
+func FreeCString(ptr *C.char) {
+	C.free(unsafe.Pointer(ptr))
+}
 
 func EcdsaKeyGen() []string {
 	return keygen.NewEcdsaKeyGen()
 }
 
 //export EcdsaKeyGenSimple
-func EcdsaKeyGenSimple() (string, string, string) {
+func EcdsaKeyGenSimple() (*C.char, *C.char, *C.char) {
 	shares := keygen.NewEcdsaKeyGen()
-	return shares[0], shares[1], shares[2]
+	share0, share1, share2 := shares[0], shares[1], shares[2]
+
+	var p0, p1, p2 runtime.Pinner
+	p0.Pin(unsafe.Pointer(&share0))
+	p1.Pin(unsafe.Pointer(&share1))
+	p2.Pin(unsafe.Pointer(&share2))
+
+	defer p0.Unpin()
+	defer p1.Unpin()
+	defer p2.Unpin()
+
+	return C.CString(share0), C.CString(share1), C.CString(share2)
 }
 
 func EcdsaRefresh(share1st string, share2nd string) ([]string, error) {
